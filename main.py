ef13115bc4758d9f91b7e5e3e7922889ed1d0c11
cyberburick <cyberburick@gmail.com>
2021-08-18 15:42:13 +0300

    Fix

--- main.py
+++ main.py
@@ -37,24 +37,24 @@ def say_welcome(message):
         parse_mode='html'
     )
 
+#
+# @bot.message_handler(func=lambda message: True)
+# def echo(message):
+#     for t, resp in dialog.items():
+#         if sum([e in message.text.lower() for e in resp['in']]):
+#             logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used {t}:\n\n%s', message.text)
+#             bot.send_message(message.chat.id, random.choice(resp['out']))
+#             return
+#
+#     logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used echo:\n\n%s', message.text)
+#     bot.send_message(message.chat.id, message.text)
+#
+# @bot.message_handler(commands=["id"])
+# def get_id(message):
+#     logger.info(f'</code>@{message.from_user.username}<code> used /id')
+#     bot.send_message(message.chat.id, f"user_id = {message.chat.id}")
 
-@bot.message_handler(func=lambda message: True)
-def echo(message):
-    for t, resp in dialog.items():
-        if sum([e in message.text.lower() for e in resp['in']]):
-            logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used {t}:\n\n%s', message.text)
-            bot.send_message(message.chat.id, random.choice(resp['out']))
-            return
-
-    logger.info(f'</code>@{message.from_user.username}<code> ({message.chat.id}) used echo:\n\n%s', message.text)
-    bot.send_message(message.chat.id, message.text)
-
-@bot.message_handler(commands=["id"])
-def get_id(message):
-    logger.info(f'</code>@{message.from_user.username}<code> used /id')
-    bot.send_message(message.chat.id, f"user_id = {message.chat.id}")
-
-@bot.message_handler(commands=["test"])
+@bot.message_handler(commands=['test'])
 def get_id(message):
     test = 5
     while test:
